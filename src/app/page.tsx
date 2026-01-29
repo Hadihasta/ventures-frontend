25af16db5925e2cbb5f7e910831e5d00c761ef8a
hadiasta <hijrihadi@gmail.com>
2026-01-29 23:53:18 +0700

    feat : initialize project delete unused file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,52 @@
-import Image from "next/image";
 
-export default function Home() {
+import Link from "next/link"
+import { ClipboardList, FileText } from "lucide-react"
+
+export default function DirectionSection() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="min-h-screen flex items-center justify-center bg-[#f5f9ff] px-6">
+      <div className="grid md:grid-cols-2 gap-10 max-w-5xl w-full">
+
+        <Link id="todos_card" href="/todos" className="group">
+          <div className="relative p-8 rounded-3xl bg-white shadow-md transition-all duration-300         hover:-translate-y-4 hover:shadow-2xl border border-blue-100 overflow-hidden">
+
+            <div className="w-16 h-16 flex items-center justify-center rounded-2xl bg-blue-100 mb-6">
+              <ClipboardList className="text-blue-600" size={32} />
+            </div>
+
+            <h2 className="text-2xl font-bold text-blue-700 mb-2">
+              Todo App
+            </h2>
+
+            <p className="text-gray-500 opacity-0 translate-y-4  group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300">
+            add your new task and complete it
+            </p>
+
+            <div className="absolute -bottom-10 -right-10 w-40 h-40 bg-blue-200 rounded-full blur-3xl opacity-0 group-hover:opacity-40 transition-all"></div>
+          </div>
+        </Link>
+
+        {/* POST CARD */}
+        <Link id="post_card"  href="/post" className="group">
+          <div className="relative p-8 rounded-3xl bg-white shadow-md transition-all duration-300  hover:-translate-y-4 hover:shadow-2xl border border-blue-100 overflow-hidden">
+
+            <div className="w-16 h-16 flex items-center justify-center rounded-2xl bg-teal-100 mb-6">
+              <FileText className="text-teal-600" size={32} />
+            </div>
+
+            <h2 className="text-2xl font-bold text-blue-700 mb-2">
+              Post App
+            </h2>
+
+            <p className="text-gray-500 opacity-0 translate-y-4   group-hover:opacity-100 group-hover:translate-y-0   transition-all duration-300">
+            Take a look  people post list.
+            </p>
+
+            <div className="absolute -bottom-10 -right-10 w-40 h-40 bg-teal-200 rounded-full blur-3xl opacity-0 group-hover:opacity-40 transition-all"></div>
+          </div>
+        </Link>
+
+      </div>
     </div>
-  );
+  )
 }
